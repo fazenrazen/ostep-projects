42f61598c73dfde90ffb9cf4b91dad55f7ad63d3
fazenrazen <faizandarsot@gmail.com>
2026-02-19 23:44:19 -0700

    first successful unix util

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// i made a comment with a bug
-
 int BUFLEN = 1000;
 
 int main(int argc, char *argv[]) {
@@ -14,8 +12,6 @@ int main(int argc, char *argv[]) {
     for(int file = 1; file < num_files; file++) {
         // iterate through all lines - end at EOF
         FILE *fp = fopen(argv[file], "r");
-
-        feof(fp); // this is api contract UB that is hard to detect with San
 
         // check if NULL is error
         if(fp == NULL) {
