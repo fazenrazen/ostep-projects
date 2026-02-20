933e33ada8d40fbe5b3cff472fa1e1829d45f8cc
fazenrazen <faizandarsot@gmail.com>
2026-02-19 23:43:38 -0700

    example of bug

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// i made a comment with a bug
+
 int BUFLEN = 1000;
 
 int main(int argc, char *argv[]) {
@@ -12,6 +14,8 @@ int main(int argc, char *argv[]) {
     for(int file = 1; file < num_files; file++) {
         // iterate through all lines - end at EOF
         FILE *fp = fopen(argv[file], "r");
+
+        feof(fp); // this is api contract UB that is hard to detect with San
 
         // check if NULL is error
         if(fp == NULL) {
